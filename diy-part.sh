77d8a9cc1789de0c0bfd84ee4ee57f8c6a5bb820
cadier <ma3wen2bin1@gmail.com>
2026-05-04 17:51:21 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -6,3 +6,9 @@
 
 # 默认开启 WiFi
 sed -i "s/option disabled '1'/option disabled '0'/g" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+
+cat << 'EOF' >> package/base-files/files/etc/uci-defaults/99-default-lang
+uci set luci.main.lang='zh_cn'
+uci commit luci
+exit 0
+EOF
