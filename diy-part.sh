eaf398bcb40cf6e37cdd7a36a35a04c865b2afed
cadier <ma3wen2bin1@gmail.com>
2026-05-04 15:46:35 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -3,3 +3,6 @@
 # 更新并安装 feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
+# 默认开启 WiFi
+sed -i "s/option disabled '1'/option disabled '0'/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
