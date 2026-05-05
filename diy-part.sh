68ba105a5dafe4154bf9b93660ec2b0b8c9b22ab
cadier <ma3wen2bin1@gmail.com>
2026-05-05 14:44:26 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -13,7 +13,7 @@
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-# ===== 默认开启 WiFi =====
+# ===== 默认开启 WiFi (开源驱动通用) =====
 sed -i "s/option disabled '1'/option disabled '0'/g" \
 package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
